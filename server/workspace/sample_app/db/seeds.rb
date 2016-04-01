7a7264e885053c1bb6a8a46eeb88a9eaea396bd6
oomori-you <valsamina@gmail.com>
2016-04-01 15:38:16 +0900

    チュートリアル10.1

--- server/workspace/sample_app/db/seeds.rb
+++ server/workspace/sample_app/db/seeds.rb
@@ -3,7 +3,9 @@ User.create!(
   email: "example@railstutorial.org",
   password: "foobar",
   password_confirmation: "foobar",
-	admin: true
+	admin: true,
+	activated: true,
+	activated_at: Time.zone.now
 )
 
 99.times do |n|
@@ -14,7 +16,9 @@ User.create!(
     name: name,
     email: email,
     password: password,
-    password_confirmation: password
+    password_confirmation: password,
+		activated: true,
+		activated_at: Time.zone.now
   )
 end
 
